6cd32f69edf6c42736f4b8af3d387aa54c6230fa
Seiichi Sugahara <poipoipoi33378@gmail.com>
2018-08-24 12:03:24 +0900

    Finish lesson4

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -5,15 +5,19 @@ FactoryBot.define do
     due_on 1.week.from_now
     association :owner
 
-    factory :project_due_yesterday do
+    trait :with_notes do
+      after(:create){|project| create_list(:note,5,project: project)}
+    end
+
+    trait :project_due_yesterday do
       due_on 1.day.ago
     end
 
-    factory :project_due_today do
+    trait :project_due_today do
       due_on Date.current.in_time_zone
     end
 
-    factory :project_due_tomorrow do
+    trait :project_due_tomorrow do
       due_on 1.day.from_now
     end
 
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,29 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
+
+  it "can have many notes" do
+    project = FactoryBot.create(:project, :with_notes)
+    expect(project.notes.length).to eq 5
+  end
+
   describe "late ststus" do
     it "is late when the due date is past today" do
-      project = FactoryBot.create(:project_due_yesterday)
+      project = FactoryBot.create(:project,:project_due_yesterday)
       expect(project).to be_late
     end
 
     it "is on time when the due date is today" do
-      project = FactoryBot.create(:project_due_today)
+      project = FactoryBot.create(:project,:project_due_today)
       expect(project).to_not be_late
     end
 
     it "is on time when the due date is in the future" do
-      project = FactoryBot.create(:project_due_tomorrow)
+      project = FactoryBot.create(:project,:project_due_tomorrow)
       expect(project).to_not be_late
     end
 
   end
 
   before do
-    @user = User.create(first_name:  "Joe",
-                       last_name:   "Tester",
-                       email:       "joeester@example,com",
-                       password:    "dottle-nouveau-pavilion-tights-furze")
+    @user = FactoryBot.create(:user)
     @user.projects.create(name: "Test Project")
   end
 
@@ -34,10 +37,7 @@ RSpec.describe Project, type: :model do
   end
 
   it "allows two users to share a project name" do
-    other_user = User.create(first_name:  "Jane",
-                       last_name:   "Tester",
-                       email:       "janeterer@example,com",
-                       password:    "dottle-nouveau-pavilion-tights-furze")
+    other_user = FactoryBot.create(:user)
     other_user.projects.create(name:"Test Project")
 
     expect(other_user).to be_valid
