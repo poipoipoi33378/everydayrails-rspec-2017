df1739e115cf23566665ff7c20a4035b2a34b80b
Seiichi Sugahara <poipoipoi33378@gmail.com>
2018-08-24 20:50:26 +0900

    Finish lesson5

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -60,18 +60,32 @@ RSpec.describe ProjectsController, type: :controller do
         expect(response).to redirect_to root_path
       end
     end
+  end
 
+  describe "#create" do
     context "as an authenticated user" do
       before do
         @user = FactoryBot.create(:user)
       end
 
-      it "adds a project" do
-        project_params = FactoryBot.attributes_for(:project)
-        sign_in @user
-        expect do
-          post :create, params: { project: project_params }
-        end.to change(@user.projects, :count).by(1)
+      context "with valid attributes" do
+        it "adds a project" do
+          project_params = FactoryBot.attributes_for(:project)
+          sign_in @user
+          expect do
+            post :create, params: { project: project_params }
+          end.to change(@user.projects, :count).by(1)
+        end
+      end
+
+      context "with invalid attributes" do
+        it "does not add a project" do
+          project_params = FactoryBot.attributes_for(:project, :invalid)
+          sign_in @user
+          expect do
+            post :create, params: { project: project_params }
+          end.to_not change(@user.projects, :count)
+        end
       end
     end
 
